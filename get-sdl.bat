2b74472d2454a0fc6550810e606fd1b31f8d82fa
Keshizin <fab.takeshi@gmail.com>
2023-01-27 12:13:47 -0300

    Fixing the relative path in bat/py scripts and including bin directory to output

--- get-sdl.bat
+++ get-sdl.bat
@@ -1,11 +1,16 @@
-py get-sdl.py
-cd SDL2-2.26.2/VisualC
+@SET PROJ_PATH=%cd%
+@SET SDL2_VERSION=SDL2-2.26.2
+
+py get-sdl.py %SDL2_VERSION%
+cd %SDL2_VERSION%/VisualC
 msbuild SDL.sln /p:Configuration=Release /p:Platform=x64
 
-copy "C:\Takeshi\Projetos\sdl-playground\SDL2-2.26.2\VisualC\x64\Release\SDL2.lib" "C:\Takeshi\Projetos\sdl-playground\lib"
-copy "C:\Takeshi\Projetos\sdl-playground\SDL2-2.26.2\VisualC\x64\Release\SDL2main.lib" "C:\Takeshi\Projetos\sdl-playground\lib"
-copy "C:\Takeshi\Projetos\sdl-playground\SDL2-2.26.2\VisualC\x64\Release\SDL2.dll" "C:\Takeshi\Projetos\sdl-playground\lib"
+copy "%PROJ_PATH%\%SDL2_VERSION%\VisualC\x64\Release\SDL2.lib" "%PROJ_PATH%\lib"
+copy "%PROJ_PATH%\%SDL2_VERSION%\VisualC\x64\Release\SDL2main.lib" "%PROJ_PATH%\lib"
+copy "%PROJ_PATH%\%SDL2_VERSION%\VisualC\x64\Release\SDL2.dll" "%PROJ_PATH%\bin"
+copy "%PROJ_PATH%\%SDL2_VERSION%\include\" "%PROJ_PATH%\include\SDL2\"
 
-copy "C:\Takeshi\Projetos\sdl-playground\SDL2-2.26.2\include\" "C:\Takeshi\Projetos\sdl-playground\include\SDL2\"
+cd %PROJ_PATH%
 
-cd "..\..\"
+del %SDL2_VERSION%.zip
+@rmdir /s /q %SDL2_VERSION%
--- get-sdl.py
+++ get-sdl.py
@@ -1,6 +1,7 @@
 import wget
 from zipfile import ZipFile
 import os
+import sys
 
 sdl_version = '2.26.2'
 sdl_name = 'SDL2-' + sdl_version + '.zip'
@@ -8,6 +9,11 @@ sdl_name = 'SDL2-' + sdl_version + '.zip'
 url = 'https://github.com/libsdl-org/SDL/releases/download/release-' + sdl_version + '/' + sdl_name
 
 def main():
+	global sdl_version
+
+	if(len(sys.argv) > 1 and isinstance(sys.argv[1], str)):
+		sdl_version = sys.argv[1]
+
 	# download SDL 2.26.2
 	print('downloading SDL ' + sdl_version)
 	wget.download(url)
